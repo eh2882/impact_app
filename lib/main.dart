19c6d2b5c7e2e857921718118048bd9d52692e79
Ethan Huang <ethanhuang@ethans-mbp.lan>
2021-11-25 10:44:08 -0800

    Updated impact home page: improved layout setup -> using rows() columns() adjusted existing donut chart added milestone widget added recent history widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 void main() {
   runApp(MyApp());
@@ -59,51 +60,270 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Container(
         child: Scaffold(
-            body: SfCircularChart(
-      annotations: <CircularChartAnnotation>[
-        CircularChartAnnotation(
-            widget: Container(child: const Text("\n dollars \n\n hours")),
-            radius: '0%',
-            verticalAlignment: ChartAlignment.center)
-        //horizontalAlignment: ChartAlignment.far)
-      ],
-      title: ChartTitle(text: 'Hi Daniel, \n here is your impact summary'),
-      series: <CircularSeries>[
-        DoughnutSeries<Nonprofits, String>(
-            dataSource: getChartData(),
-            xValueMapper: (Nonprofits data, _) => data.org,
-            yValueMapper: (Nonprofits data, _) => data.impact,
-            dataLabelSettings: DataLabelSettings(isVisible: true),
-            explode: true,
-            explodeAll: true,
-            explodeOffset: "2%")
-        //enableTooltip: true)
-        //maximumValue: 40000)
-      ],
-      legend: Legend(
-        isVisible: true,
-        overflowMode: LegendItemOverflowMode.wrap,
-        position: LegendPosition.bottom,
-      ),
-      tooltipBehavior: _tooltipBehavior,
-    )));
+            body: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+          SizedBox(height: 40),
+          Container(
+            child: Text("Hi Daniel,\nhere's your impact summary",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
+          ),
+          SizedBox(height: 10),
+          Container(
+              height: 400,
+              width: 500,
+              child: SfCircularChart(
+                margin: EdgeInsets.all(0),
+                annotations: <CircularChartAnnotation>[
+                  CircularChartAnnotation(
+                      widget: Container(
+                          child: const Text("\n dollars \n\n hours",
+                              style: TextStyle(fontSize: 15))),
+                      radius: '0%',
+                      verticalAlignment: ChartAlignment.center)
+                  //horizontalAlignment: ChartAlignment.far)
+                ],
+                series: <CircularSeries>[
+                  DoughnutSeries<Nonprofits, String>(
+                      dataSource: getChartData(),
+                      pointColorMapper: (Nonprofits data, _) => data.color,
+                      xValueMapper: (Nonprofits data, _) => data.org,
+                      yValueMapper: (Nonprofits data, _) => data.impact,
+                      dataLabelSettings: DataLabelSettings(isVisible: true),
+                      explode: true,
+                      explodeAll: true,
+                      explodeOffset: "2%")
+                  //enableTooltip: true)
+                  //maximumValue: 40000)
+                ],
+                legend: Legend(
+                    padding: 12,
+                    isVisible: true,
+                    overflowMode: LegendItemOverflowMode.wrap,
+                    position: LegendPosition.bottom,
+                    textStyle: TextStyle(fontSize: 14)),
+                tooltipBehavior: _tooltipBehavior,
+              )),
+          SizedBox(height: 20),
+          Container(
+            child: Text("Milestone Breakdown",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700)),
+          ),
+          SizedBox(height: 20),
+          Container(
+              height: 250,
+              width: 400,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                color: Colors.orangeAccent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  //crossAxisAlignment: CrossAxisAlignment.baseline,
+                  //textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    SizedBox(height: 40),
+                    Row(children: [
+                      SizedBox(width: 25),
+                      Text(
+                        'Volunteer',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(width: 75),
+                      Text(
+                        'Remaining',
+                        style: TextStyle(
+                            color: Colors.brown[900],
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]),
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        SizedBox(width: 40),
+                        Text('40.0',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w700)),
+                        SizedBox(width: 15),
+                        Text('hours',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            )),
+                        SizedBox(width: 90),
+                        Text('20.0',
+                            style: TextStyle(
+                                color: Colors.brown[900],
+                                fontSize: 35,
+                                fontWeight: FontWeight.w700)),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Stack(fit: StackFit.loose, children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width: 370,
+                        child: Center(
+                          child: SfLinearGauge(
+                            axisTrackStyle: LinearAxisTrackStyle(
+                              thickness: 25,
+                              edgeStyle: LinearEdgeStyle.bothCurve,
+                              color: Colors.brown[900],
+                            ),
+                            showLabels: false,
+                            showTicks: false,
+                            barPointers: [
+                              LinearBarPointer(
+                                  thickness: 25,
+                                  value: 80,
+                                  edgeStyle: LinearEdgeStyle.bothCurve,
+                                  //Change the color
+                                  color: Colors.white)
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 200),
+                      Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 200,
+                          child: Text('2 weeks left to complete!',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500))),
+                      Column(children: [
+                        Container(
+                            alignment: Alignment.centerRight,
+                            height: 50,
+                            width: 350,
+                            child: Text('80%',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500))),
+                      ]),
+                    ]),
+                    Stack(
+                      children: [
+                        SizedBox(width: 50),
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text('monthly goal\t\t60 dollars',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          SizedBox(height: 20),
+          Container(
+            child: Text("Recent History",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700)),
+          ),
+          SizedBox(height: 20),
+          Container(
+              height: 400,
+              //padding: new EdgeInsets.(5.0),
+              //alignment: Alignment.center,
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.white70,
+                  elevation: 10,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        title: const Text('Autism Speaks',
+                            style: TextStyle(fontWeight: FontWeight.w500)),
+                        leading: Icon(Icons.extension_rounded),
+                        subtitle: const Text('Nov 15, 2021'),
+                        trailing: const Text('3 hours',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                                color: Colors.orange)),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const Text('Action in Africa',
+                            style: TextStyle(fontWeight: FontWeight.w500)),
+                        leading: Icon(Icons.support),
+                        subtitle: const Text('Oct 25, 2021'),
+                        trailing: const Text('20 dollars',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                                color: Colors.green)),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const Text('Lifewater',
+                            style: TextStyle(fontWeight: FontWeight.w500)),
+                        leading: Icon(Icons.water),
+                        subtitle: const Text('Sept 15, 2021'),
+                        trailing: const Text('5 hours',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                                color: Colors.orange)),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const Text('World Concern',
+                            style: TextStyle(fontWeight: FontWeight.w500)),
+                        leading: Icon(Icons.public),
+                        subtitle: const Text('Sept 12, 2021'),
+                        trailing: const Text('50 dollars',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                                color: Colors.green)),
+                      ),
+                    ],
+                  )))
+        ]))));
   }
 
   List<Nonprofits> getChartData() {
     final List<Nonprofits> chartData = [
-      Nonprofits('Education', 50),
-      Nonprofits('Environment', 5),
-      Nonprofits('Health', 15),
-      Nonprofits('Cultural', 25),
+      Nonprofits('Education', 50, Colors.deepPurple),
+      Nonprofits('Environment', 5, Colors.yellowAccent),
+      Nonprofits('Health', 15, Colors.teal),
+      Nonprofits('Cultural', 25, Colors.red)
     ];
     return chartData;
   }
 }
 
 class Nonprofits {
-  Nonprofits(this.org, this.impact);
+  Nonprofits(this.org, this.impact, this.color);
   final String org;
   final int impact;
+  final Color color;
 }
